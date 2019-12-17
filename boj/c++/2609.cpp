eaf1add4fc7ff663819af760cba4b2e69cc9fc8b
DoLim <limdongyoung0@gmail.com>
2019-12-17 13:54:15 +0000

    2609 fix

--- boj/c++/2609.cpp
+++ boj/c++/2609.cpp
@@ -1,1 +1,31 @@
-1934
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (a < b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    int n = 0;
+    while (b != 0) {
+        n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    int g = gcd(a, b);
+    int l = a * b / g;
+    cout << g << '\n';
+    cout << l << '\n';
+
+    return 0;
+}

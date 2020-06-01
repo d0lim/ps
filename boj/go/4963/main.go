b19f59ec0f7117bc28cb59bc5f91c5e86fc549b9
DoLim <limdongyoung0@gmail.com>
2020-06-01 14:47:25 +0000

    feat: get appropriate input

--- boj/go/4963/main.go
+++ boj/go/4963/main.go
@@ -8,8 +8,28 @@ import (
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
-	var w int
-	var h int
-	fmt.Fscan(r, &w, &h)
-	fmt.Println("w, h is", w, h)
+	for {
+		input := [51][51]int{}
+		var w int
+		var h int
+		fmt.Fscan(r, &w, &h)
+		// fmt.Println("w, h is", w, h)
+		// fmt.Printf("%T\n", w)
+		// fmt.Printf("%T\n", h)
+
+		for i := 0; i < w; i++ {
+			for j := 0; j < h; j++ {
+				fmt.Fscan(r, &input[i][j])
+			}
+		}
+
+		for i := 0; i < w; i++ {
+			for j := 0; j < h; j++ {
+				fmt.Printf("%d ", input[i][j])
+			}
+			fmt.Println()
+		}
+
+	}
+
 }

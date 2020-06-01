e00cd5eed5867fbd7e983d0b9923ddc977135bee
DoLim <limdongyoung0@gmail.com>
2020-06-01 14:09:13 +0000

    feat: wrote some print util func

--- boj/go/4963/main.go
+++ boj/go/4963/main.go
@@ -5,22 +5,38 @@ import (
 	"os"
 )
 
+func bufioPrintln(w *bufio.Writer, line string) {
+	w.Write([]byte(line))
+	w.Write([]byte("\n"))
+	w.Flush()
+}
+
+func bufioPrintlnBytes(w *bufio.Writer, bytes []byte) {
+	w.Write(bytes)
+	w.Write([]byte("\n"))
+	w.Flush()
+}
+
+func bufioPrintBytes(w *bufio.Writer, bytes []byte) {
+	w.Write(bytes)
+	w.Flush()
+}
+
+func bufioPrintError(w *bufio.Writer, err error) {
+	w.Write([]byte(err.Error()))
+	w.Flush()
+}
+
 func main() {
 	w := bufio.NewWriter(os.Stdout)
-
 	r := bufio.NewReader(os.Stdin)
 
 	line, isPrefix, err := r.ReadLine()
 	if err != nil {
-		w.Write([]byte(err.Error()))
-		w.Flush()
+		bufioPrintError(w, err)
 	}
 	if !isPrefix {
-		w.Write(line)
-		w.Write([]byte("\n"))
-		w.Flush()
+		bufioPrintlnBytes(w, line)
 	}
-	w.Write([]byte("Haha\n"))
-	w.Flush()
 
 }

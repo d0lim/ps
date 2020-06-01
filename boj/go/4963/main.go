5663baeb129104562d3cbcd2b2df32cd27f8c841
DoLim <limdongyoung0@gmail.com>
2020-06-01 14:30:14 +0000

    feat: simplified io

--- boj/go/4963/main.go
+++ boj/go/4963/main.go
@@ -2,41 +2,14 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
-func bufioPrintln(w *bufio.Writer, line string) {
-	w.Write([]byte(line))
-	w.Write([]byte("\n"))
-	w.Flush()
-}
-
-func bufioPrintlnBytes(w *bufio.Writer, bytes []byte) {
-	w.Write(bytes)
-	w.Write([]byte("\n"))
-	w.Flush()
-}
-
-func bufioPrintBytes(w *bufio.Writer, bytes []byte) {
-	w.Write(bytes)
-	w.Flush()
-}
-
-func bufioPrintError(w *bufio.Writer, err error) {
-	w.Write([]byte(err.Error()))
-	w.Flush()
-}
-
 func main() {
-	w := bufio.NewWriter(os.Stdout)
 	r := bufio.NewReader(os.Stdin)
-
-	line, isPrefix, err := r.ReadLine()
-	if err != nil {
-		bufioPrintError(w, err)
-	}
-	if !isPrefix {
-		bufioPrintlnBytes(w, line)
-	}
-
+	var w int
+	var h int
+	fmt.Fscan(r, &w, &h)
+	fmt.Println("w, h is", w, h)
 }

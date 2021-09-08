466955f4a2ccb28906259cf5ded61cbe3a0b8fa0
d0lim <limdongyoung0@gmail.com>
2021-09-09 01:17:08 +0900

    solve: kakao 2021 03

--- kakao/2021/03.py
+++ kakao/2021/03.py
@@ -1,31 +1,45 @@
+from bisect import bisect_left
+from collections import defaultdict
+from itertools import combinations
 
 
 def solution(info, query):
-    answer = [0] * len(query)
+    answer = []
+    dic = {}
+    comb = [0, 1, 2, 3]
+    for i in info:
+        person = i.split()
+        conditions = person[:-1]
+        score = int(person[-1])
+        for j in range(5):
+            for k in list(combinations(comb, j)):
+                temp = conditions.copy()
+                for idx in k:
+                    temp[idx] = '-'
+                key = ''.join(temp)
+                if key in dic:
+                    dic[key].append(score)
+                else:
+                    dic[key] = [score]
+    # print(dic)
+    for value in dic.values(): 
+        value.sort()
 
-    dic = {"java": set(), "cpp": set(), "python": set(), "backend": set(), "frontend": set(), "junior": set(), "senior": set(), "chicken": set(), "pizza": set()}
-    for index, i in enumerate(info):
-        splitted_info = i.split()
-        dic[splitted_info[0]].add(index)
-        dic[splitted_info[1]].add(index)
-        dic[splitted_info[2]].add(index)
-        dic[splitted_info[3]].add(index)
-    for index, q in enumerate(query):
-        splitted_query = q.split(' and ')
-        splitted_query = splitted_query[:-1] + splitted_query[-1].split()
-        q_test = int(splitted_query[4])
+    for q in query:
+        q_list = q.split(' and ')
+        q_list = q_list[:-1] + q_list[-1].split()
 
-        search_set = set([i for i in range(len(info))])
-        for i in range(4):
-            if splitted_query[i] != '-':
-                search_set = search_set & dic[splitted_query[i]]
+        target = int(q_list[-1])
+        key = ''.join(q_list[:-1])
 
-        for s in search_set:
-            if int(info[s].split()[4]) >= q_test:
-                answer[index] += 1
+        if key in dic:
+            score_list = dic[key]
 
+            index = bisect_left(score_list, target)
+            answer.append(len(score_list) - index)
+        else:
+            answer.append(0)
+            continue
     return answer
-        
-
 
 print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))

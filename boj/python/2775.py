dcaba8d8a6e724b1ef14c6afebc35fddaaaa6f30
d0lim <limdongyoung0@gmail.com>
2022-02-15 23:04:37 +0900

    refactor: boj 2775

--- boj/python/2775.py
+++ boj/python/2775.py
@@ -2,17 +2,17 @@ import sys
 
 T = int(sys.stdin.readline().rstrip())
 
+dp = [
+    list([0 for _ in range(15)]) if f != 0 else list([r for r in range(15)])
+    for f in range(15)
+]
+
+for l in range(1, 15):
+    for j in range(1, 15):
+        dp[l][j] = sum(dp[l - 1][1 : j + 1])
+
 for _ in range(T):
     k = int(sys.stdin.readline().rstrip())
     i = int(sys.stdin.readline().rstrip())
-
-    dp = [
-        list([0 for _ in range(i + 1)]) if f != 0 else list([r for r in range(i + 1)])
-        for f in range(k + 1)
-    ]
-
-    for l in range(1, k + 1):
-        for j in range(1, i + 1):
-            dp[l][j] = sum(dp[l - 1][1 : j + 1])
 
     print(dp[k][i])

f583e070e84a9ddca9396fbf86af420983f071e7
Dongyoung Lim <limdongyoung0@gmail.com>
2022-03-27 18:06:47 +0900

    refactor: 1003 from input to sys readline

--- boj/python/1003.py
+++ boj/python/1003.py
@@ -1,6 +1,8 @@
+import sys
+
 dp = [list([0, 0]) for i in range(41)]
 
-T = int(input())
+T = int(sys.stdin.readline().rstrip())
 
 dp[0] = [1, 0]
 dp[1] = [0, 1]
@@ -9,5 +11,5 @@ for i in range(2, 41):
     dp[i] = [dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1]]
 
 for j in range(T):
-    n = int(input())
+    n = int(sys.stdin.readline().rstrip())
     print(dp[n][0], dp[n][1])

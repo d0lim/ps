563ec677552a863281203b45bd72232c48baecf2
Dongyoung Lim <limdongyoung0@gmail.com>
2022-05-08 21:50:52 +0900

    solve: boj 1697 with bfs

--- boj/python/1697.py
+++ boj/python/1697.py
@@ -1,24 +1,31 @@
 import sys
+from collections import deque
 
 N, K = map(int, sys.stdin.readline().rstrip().split())
 
-dp = [100000] * 100002
-dp[N] = 0
-j = 1
-for i in range(N - 1, -1, -1):
-    dp[i] = j
-    j += 1
+visited = [False] * 100001
 
-for i in range(N + 1, 100001):
-    if i % 2 == 0:
-        dp[i] = min(dp[i // 2], dp[i - 1], dp[i + 1]) + 1
-    else:
-        dp[i] = min(dp[i - 1], dp[i + 1]) + 1
+q = deque()
 
-for i in range(N + 1, 100001):
-    if i % 2 == 0:
-        dp[i] = min(dp[i // 2], dp[i - 1], dp[i + 1]) + 1
-    else:
-        dp[i] = min(dp[i - 1], dp[i + 1]) + 1
+q.append(N)
+visited[N] = True
 
-print(dp[K])
+t = [0] * 100001
+while q:
+    current = q.popleft()
+
+    available = list(
+        filter(lambda x: 0 <= x <= 100000, [current - 1, current + 1, current * 2])
+    )
+
+    for next_position in available:
+        if not visited[next_position]:
+            visited[next_position] = True
+            q.append(next_position)
+            t[next_position] = t[current] + 1
+
+        if next_position == K:
+            q = []
+            break
+
+print(t[K])

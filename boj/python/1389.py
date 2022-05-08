7235c3edd1ab1571fad2f4bc97ed14adb0542c64
Dongyoung Lim <limdongyoung0@gmail.com>
2022-05-08 21:39:08 +0900

    solve: boj 1389 with better logic

--- boj/python/1389.py
+++ boj/python/1389.py
@@ -1,38 +1,38 @@
 import sys
+from collections import defaultdict, deque
 
 N, M = map(int, sys.stdin.readline().rstrip().split())
 
-matrix = [[0] * (N + 1) for _ in range(N + 1)]
+relations = defaultdict(list)
 
 for _ in range(M):
-    a, b = map(int, sys.stdin.readline().rstrip().split())
-    matrix[a][b] = 1
-    matrix[b][a] = 1
+    A, B = map(int, sys.stdin.readline().rstrip().split())
+    relations[A].append(B)
+    relations[B].append(A)
 
-for _ in range(N):
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
-            if i == j:
-                continue
-            if matrix[i][j] == 1:
-                continue
-            else:
-                for y in [x for x, t in enumerate(matrix[j]) if matrix[j][x] != 0]:
-                    if matrix[i][y] != 0:
-                        if matrix[i][j] == 0:
-                            matrix[i][j] = matrix[i][y] + matrix[j][y]
-                        else:
-                            matrix[i][j] = min(
-                                matrix[i][j], matrix[i][y] + matrix[j][y]
-                            )
+result = []
+for i in range(1, N + 1):
+    visited = [False] * (N + 1)
+    q = deque()
+    q.append(i)
+    visited[i] = True
+    count = [0] * (N + 1)
+    while q:
+        current = q.popleft()
 
-sol = 0
-ans = 999999999
-for idx, row in enumerate(matrix):
-    if idx == 0:
-        continue
-    if ans > sum(row):
-        ans = sum(row)
-        sol = idx
+        for friend in relations[current]:
+            if not visited[friend]:
+                q.append(friend)
+                visited[friend] = True
+                count[friend] = count[current] + 1
 
-print(sol)
+    result.append(sum(count))
+
+insider = 0
+insider_value = result[0]
+for i, v in enumerate(result):
+    if v < insider_value:
+        insider = i
+        insider_value = v
+
+print(insider + 1)

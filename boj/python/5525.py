c6fa9ea00568b571cb76f2a58b38030227c2dd20
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-19 22:36:07 +0900

    solve: boj 5525 better solution

--- boj/python/5525.py
+++ boj/python/5525.py
@@ -1,56 +1,21 @@
-import sys
+N = int(input())
+M = int(input())
+S = input()
 
+count = 0
+pattern = 0
+i = 1
+while i < M - 1:
+    if S[i - 1] == "I" and S[i] == "O" and S[i + 1] == "I":
+        pattern += 1
+        if pattern == N:
+            # 또 만족할 수 있음
+            pattern -= 1
+            count += 1
+        # 2번 뛰기
+        i += 1
+    else:
+        pattern = 0
+    i += 1
 
-def compute_lps(pat, lps):
-    l = 0
-    i = 1
-    # lps[0] == 0
-    while i < len(pat):
-        if pat[i] == pat[l]:
-            l += 1
-            lps[i] = l
-            i += 1
-        else:
-            if l != 0:
-                l = lps[l - 1]
-            else:
-                lps[i] = 0
-                i += 1
-    return lps
-
-
-def kmp_search(pat, txt):
-    m = len(pat)
-    n = len(txt)
-
-    lps = [0] * m
-
-    lps = compute_lps(pat, lps)
-
-    i = 0
-    j = 0
-    cnt = 0
-    while i < n:
-        if pat[j] == txt[i]:
-            i += 1
-            j += 1
-        elif pat[j] != txt[i]:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                i += 1
-
-        if j == m:
-            cnt += 1
-            j = lps[j - 1]
-
-    return cnt
-
-
-N = int(sys.stdin.readline().rstrip())
-_ = sys.stdin.readline()
-S = sys.stdin.readline().rstrip()
-
-P = "IO" * N + "I"
-
-print(kmp_search(P, S))
+print(count)
